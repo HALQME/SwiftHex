eb16a956383100b5306d131db5b8897a0f5f4b41
HAL <68320771+HALQME@users.noreply.github.com>
2024-09-19 08:38:44 +0900

    init

--- Sources/SwiftHex/SwiftHex.swift
+++ Sources/SwiftHex/SwiftHex.swift
@@ -1,2 +1,75 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public struct Hex {
+
+    @usableFromInline
+    var storage: Storage
+
+    @usableFromInline
+    final class Storage {
+        @usableFromInline var value: UInt64
+        init(_ value: UInt64) { self.value = value }
+    }
+
+    public init(_ string: String) throws {
+        guard let value = UInt64(string, radix: 16) else { throw HexError.invalidFormat }
+        self.storage = Storage(value)
+    }
+
+    public init(_ value: UInt64) {
+        self.storage = Storage(value)
+    }
+
+    public init<T: BinaryInteger>(_ value: T) {
+        self.storage = Storage(UInt64(value))
+    }
+
+    @inlinable
+    public func toUInt8() throws -> UInt8 {
+        guard storage.value <= UInt64(UInt8.max) else { throw HexError.overflow }
+        return UInt8(storage.value)
+    }
+
+    @inlinable
+    public func toUInt64() -> UInt64 {
+        return storage.value
+    }
+}
+
+extension Hex: CustomStringConvertible {
+    public var description: String {
+        return String(storage.value, radix: 16, uppercase: true)
+    }
+
+    public func description(_ count: Int) -> String {
+        let string = self.description
+        guard count > string.count else { return string }
+        let formats = String(repeating: "0", count: count - string.count)
+        return formats + string
+    }
+}
+
+extension Hex: Equatable, Hashable {
+    public static func == (lhs: Hex, rhs: Hex) -> Bool {
+        return lhs.storage.value == rhs.storage.value
+    }
+
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(storage.value)
+    }
+}
+
+extension Hex: Comparable {
+    public static func < (lhs: Hex, rhs: Hex) -> Bool {
+        return lhs.storage.value < rhs.storage.value
+    }
+    
+    public static func > (lhs: Hex, rhs: Hex) -> Bool {
+        return lhs.storage.value > rhs.storage.value
+    }
+}
+
+extension Hex {
+    public enum HexError: Error {
+        case overflow
+        case invalidFormat
+    }
+}
--- Tests/SwiftHexTests/SwiftHexTests.swift
+++ Tests/SwiftHexTests/SwiftHexTests.swift
@@ -1,6 +1,80 @@
 import Testing
 @testable import SwiftHex
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Suite("Initialize")
+struct InitTests {
+    @Test("with hex string")
+    func initializeWithHexString() {
+        #expect(try! Hex("00000").storage.value == .zero)
+        #expect(try! Hex("F").storage.value == UInt64(15))
+    }
+
+    @Test("returns nil if invalid hex string")
+    func initializeNilr() {
+        #expect(throws: Hex.HexError.invalidFormat, performing: { try Hex.init("G") })
+    }
+
+    @Test("with UInt64")
+    func initializeWithUInt64() {
+        #expect(Hex(.zero).storage.value == .zero)
+        #expect(Hex(UInt64(15)).storage.value == UInt64(15))
+    }
+
+    @Test("with BinaryInteger")
+    func initializeWithBinaryInteger() {
+        #expect(Hex(0b01).storage.value == UInt64(1))
+        #expect(Hex(0b1111).storage.value == UInt64(15))
+    }
+}
+
+@Suite("Convert")
+struct ConvertTests {
+    @Test("Description")
+    func convertToHexString() {
+        #expect(Hex(UInt64(15)).description == String("F"))
+        #expect(Hex(UInt64(255)).description == String("FF"))
+    }
+
+    @Test("String with fix length")
+    func convertToHexStringWithFixLength() {
+        #expect(Hex(UInt64(15)).description(2) == String("0F"))
+        #expect(Hex(UInt64(24)).description(3) == String("018"))
+        #expect(Hex(UInt64(255)).description(6) == String("0000FF"))
+    }
+
+    @Test("UInts convert")
+    func convertToUInt() {
+        #expect(Hex(UInt64(15)).toUInt64() == UInt64(15))
+        #expect(try! Hex(UInt64(16)).toUInt8() == UInt8(16))
+    }
+
+    func convertToUInt8Error() {
+        #expect(throws: Hex.HexError.overflow, performing: { try Hex(UInt64(256)).toUInt8() })
+    }
+}
+
+@Suite("Equatable, Hashable")
+struct Equatable_Hashable_Tests {
+    @Test("Equatable")
+    func equal() {
+        #expect(try! Hex(UInt64(15)) == Hex(String("F")))
+        #expect(Hex(UInt8(15)) == Hex(UInt64(15)))
+        #expect(Hex(UInt64(15)) != Hex(UInt64(255)))
+    }
+    @Test("Hashable")
+    func hash() {
+        #expect(try! Hex(UInt64(15)).hashValue == Hex(String("F")).hashValue)
+        #expect(Hex(UInt8(15)).hashValue == Hex(UInt64(15)).hashValue)
+    }
+}
+
+@Suite("Comparabke")
+struct Comparable_Tests {
+    @Test("Comparable")
+    func lessThan() {
+        #expect(Hex(UInt64(15)) < Hex(UInt64(255)))
+        #expect(Hex(UInt8(15)) <= Hex(UInt64(15)))
+        #expect(try! Hex(String("F")) > Hex(UInt64(13)))
+        #expect(try! Hex(String("FF")) >= Hex(UInt8.max))
+    }
 }
